90cad853eee3771df1bed1418fedb10ee9098a50
unknown <helloparadian@gmail.com>
2020-03-02 22:47:21 +0800

    'icon'

--- src/App.js
+++ src/App.js
@@ -1,52 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import DocumentTitle from 'react-document-title'
-import {Layout, Divider,ConfigProvider } from 'antd'
+import { Layout, Divider, ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 // import Header from './components/Header'
-import { connectAlita } from 'redux-alita';
+import { connectAlita } from 'redux-alita'
 import Routes from './routes'
 import SideMenu from './components/sideMenu'
 import NavBar from './components/Header'
-import './App.css';
-const {Sider,Footer,Header,Content}  = Layout
-console.log(Layout,'layout')
+import './App.css'
+const { Sider, Footer, Header, Content } = Layout
+console.log(Layout, 'layout')
 // const Footer = Layout.Footer;
-console.log(Content,'content')
-class App extends Component{
-  state={
-    title:'首页',
-    collapsed:false
+console.log(Content, 'content')
+class App extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.changeStatus = this.changeStatus.bind(this)
+  // }
+  state = {
+    title: '首页',
+    collapsed: false
   }
-  componentDidMount() {
-
-  }
-  changeStatus() {
-    this.setState ({
-      collapsed : !this.state.collapsed
+  // componentDidMount() {}
+  changeStatus = () => {
+    console.log('enter func')
+    this.setState({
+      collapsed: !this.state.collapsed
     })
   }
-  render(){
-    const {title} = this.state
-    return(
+  render() {
+    const { title } = this.state
+    return (
       <ConfigProvider locale={zhCN}>
-      <DocumentTitle title={title}>
-      <Layout>
-                <Sider>
-                    <SideMenu collapsed={this.state.collapsed}></SideMenu>
-                    </Sider>
-                <Layout style={{flexDirection:'column'}}>
-                    <Header >
-                        <NavBar commit={this.changeStatus} collapsed={this.state.collapsed}></NavBar>
-                    </Header>
-                    <Content  style={{ margin: '0 16px', overflow: 'initial', flex: '1 1 0' }}> <Routes></Routes>  </Content>
-                    <Footer>Footer</Footer>
-                </Layout>
+        <DocumentTitle title={title}>
+          <Layout>
+            <Sider>
+              <SideMenu collapsed={this.state.collapsed}></SideMenu>
+            </Sider>
+            <Layout style={{ flexDirection: 'column' }}>
+              <Header>
+                <NavBar
+                  changeStatus={this.changeStatus}
+                  collapsed={this.state.collapsed}
+                ></NavBar>
+              </Header>
+              <Content
+                style={{ margin: '0 16px', overflow: 'initial', flex: '1 1 0' }}
+              >
+                {' '}
+                <Routes></Routes>{' '}
+              </Content>
+              <Footer>Footer</Footer>
             </Layout>
-      </DocumentTitle>
+          </Layout>
+        </DocumentTitle>
       </ConfigProvider>
     )
   }
 }
 
-
-export default App;
+export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import React,{Component} from 'react'
-import {Button,Icon } from 'antd'
+import React, { Component } from 'react'
+import { Button, Icon } from 'antd'
 
 class Header extends Component {
-    constructor(props){
-        super(props)
-    }
-    state ={
-
-    }
-    render() {
-        return(
-            <div>
-             <Button>
-                 <Icon onClick={this.props.commit(),console.log('enter click event,,,,')}  type= {this.props.collapsed?'menuFoldOutlined':'menuUnfoldOutline'} />
-             </Button>
-            </div>
-        )
-    }
+  state = {}
+  componentDidMount() {}
+  render() {
+    return (
+      <div>
+        <Button onClick={this.props.changeStatus}>
+          <Icon
+            type={
+              this.props.collapsed ? 'menuFoldOutlined' : 'menuUnfoldOutline'
+            }
+          />
+        </Button>
+      </div>
+    )
+  }
 }
-export default Header;
+export default Header
