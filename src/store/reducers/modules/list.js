394b036e8d4eb39cd84d198ce11d3f2f06f364a1
paradian <helloparadian@gmail.com>
2020-03-26 17:52:09 +0800

    addition

--- src/store/reducers/modules/list.js
+++ src/store/reducers/modules/list.js
@@ -1,13 +1,22 @@
 import api from '../../../fetch/api'
+import React from 'react'
+import {Route,Redirect} from 'react-router-dom'
+import { render } from '@testing-library/react'
 
 const list = (state = [], action) => {
+  console.log(Route,'rouer')
+  //
   switch (action.type) {
+    
     case 'fetch-list':
       console.log(state)
+     
       api.getList().then(res => {
         console.log(res)
+      
         if (res.code == 200) {
           return res.data
+
         }
       })
     default:
